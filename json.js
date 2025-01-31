e97f3c4172cec03eac887ab59a6cf454a88179b5
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2025-01-31 14:36:23 +0700

    save

---
+++ json.js
@@ -0,0 +1,3 @@
+import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js";
+
+getJSON("https://nomp.mofumofu.me/api/worker_stats?sugar1qkyetyz6uypmuqaxd8hs763ymv40uk4negh30yt", null, null, ResponseFunction);
